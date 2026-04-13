c78189720f4eedab7ef10e003ce148eca55d653b
balvins435 <vincentkiplangat02@gmail.com>
2026-04-13 14:48:00 +0300

    added job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    company =models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField()
+    url = models.URLField(unique=True)
+    source = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} at {self.company}"
